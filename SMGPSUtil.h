8037a1b89253de4e9173526798694b119dfe001b
Ivan Pavlovic <ivan@spoiledmilk.com>
2014-08-05 15:54:46 +0200

    copyright

--- SMGPSUtil.h
+++ SMGPSUtil.h
@@ -3,7 +3,7 @@
 //  I Bike CPH
 //
 //  Created by Ivan Pavlovic on 05/03/2013.
-//  Copyright (C) 2013 City of Copenhagen.
+//  Copyright (C) 2013 City of Copenhagen.  All rights reserved.
 //
 //  This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
 //  If a copy of the MPL was not distributed with this file, You can obtain one at 
--- SMRouteUtils.h
+++ SMRouteUtils.h
@@ -3,7 +3,11 @@
 //  testRouteMe
 //
 //  Created by Rasko Gojkovic on 6/10/13.
-//  Copyright (c) 2013 Rasko Gojkovic. All rights reserved.
+//  Copyright (C) 2013 City of Copenhagen.
+//
+//  This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+//  If a copy of the MPL was not distributed with this file, You can obtain one at 
+//  http://mozilla.org/MPL/2.0/.
 //
 
 #import <Foundation/Foundation.h>
